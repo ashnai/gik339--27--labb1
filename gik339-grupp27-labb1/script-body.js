3f8c0d6f8e5b83ccf59411f3b23c47cb3199e7bf
ashnai <soffiloffan81@hotmail.com>
2023-11-16 14:36:32 +0100

    #Tagit bort funktionerna från hämtning

--- gik339-grupp27-labb1/script-body.js
+++ gik339-grupp27-labb1/script-body.js
@@ -1,61 +1,61 @@
-/* Hämtar div textbox med hjälp av DOM-funktion getElementsByClassName, där classname är textbox */
-function getDiv() {
-    let textbox = document.getElementsByClassName("textBox").innerHTML;
-}
-/* Hämtar input med hjälp av DOM-funktion getElementById, där id är color */
-function getColor() {
-    let color = document.getElementById("color").innerHTML;
-}
-/* Hämtar textfält med hjälp av DOM-funktion getElementByClassName, där className är textfield*/
-function getTextfield() {
-    let textfield = document.getElementByClassName("textField").innerHTML;
-}
+/* Hämtar div textbox med hjälp av getElementsByClassName, där classname är textbox */
+const textBox = document.getElementsByClassName("textBox");
+
+/* Hämtar input med hjälp av getElementById, där id är color */
+const color = document.getElementById("color");
+
+/* Hämtar textfält med hjälp getElementByClassName, där className är textfield*/
+const textfield = document.getElementsByClassName("textField");
+
 /* Hämtar content med hjälp av DOM-funktion getElementById, där id är content*/
-function getContent() {
-    let content = document.getElementById("content").innerHTML;
-}
-eventLyssnare();
-/* Hämtar textfält med hjälp av DOM-funktion getElementByClassName, där className är textfield*/
-function getTextfield1() {
-    let textfield1 = document.getElementByClassName("textField").innerHTML;
-}
-/* Hämtar divStyle med hjälp av DOM-funktion getElementById, där id är divStyle*/
-function getDivStyle() {
-    let divStyle = document.getElementById("divStyle").innerHTML;
-}
-/* Hämtar checkbox med hjälp av DOM-funktion getElementByClassName, där className är checkbox*/
-function getCheckbox() {
-    let checkbox = document.getElementByClassName("checkBox").innerHTML;
-}
-/* Hämtar knappen med hjälp av DOM-funktion getElementByTagName, där tagName är delete*/
-function getButton() {
-    let button = document.getElementsByTagName("delete").innerHTML;
-}
+const content = document.getElementById("content");
+
+/* eventLyssnare(); */
+/* Hämtar textfält med hjälp av querySelector, där className är textfield*/
+const textfield1 = document.querySelector(".textField");
+
+/* Hämtar divStyle med hjälp av getElementById, där id är divStyle*/
+const divStyle = document.getElementById("divStyle");
+
+/* Hämtar checkbox med hjälp av getElementByClassName, där className är checkbox*/
+const checkbox = document.getElementsByClassName("checkBox");
+
+/* Hämtar knappen med hjälp av getElementByTagName, där tagName är delete*/
+const button = document.querySelector("#delete");
+
 /* Skapar en funktion (eventlyssnare) som när den triggas 
 ska avsändaren (target) skrivas ut till konsolen. Om avsändaren (target) 
 är inputfältet content, ska dess värde skrivas ut till div-elementet */
 
-function eventLyssnare(event) {
+/* getTextfield1.addEventListener'click', () => {
+    content = document.getElementsByName("content");
+    content.for((content) => {
+        if (content.checked) {
+            output.innerText = `You selected: ${content.value}`;
+        }
+    }); */
+
+/* function eventLyssnare(event) {*/
     /* alert("Target")
     content.addEventListener("click", eventLyssnare); */
-    let e = event || window.event;
+    /* let e = event || window.event; */
 /* Skriver ut till konsolen vilket inputfält som genererade eventet (target) */
-    console.log("Event triggered by: " + e.content.id);
-
+   /*  console.log("Event triggered by: " + e.content.id);
+ */
 /* Tar reda på inputfältets name attribut*/
-    let fieldName = e.target.name;
-
+   /*  let fieldName = e.target.name;
+ */
     /* Om name-attributet är "content" */
-    if (fieldName == "content") {
-        let outputDiv = document.getElementById("output");
+  /*   if (fieldName == "content") {
+        let outputDiv = document.getElementById("output"); */
 
         /* Skriver ut värdet i inputfältet till div-elementet */
-        outputDiv.innerHTML = "Input value: " + e.content.value;
+       /*  outputDiv.innerHTML = "Input value: " + e.content.value;
     }
-}
+}  */
 
 /* Hämtar input-elementet */
-const contentInput = document.getElementById("content");
+/* const contentInput = document.getElementById("content"); */
 
 /* Lägger till en eventlyssnare till input-elementet */
-contentInput.addEventListener("input", handleInput);
+/* contentInput.addEventListener("input", handleInput); */
